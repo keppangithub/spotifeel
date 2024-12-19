29b1730d67d7c6ed1e3852303a557842e2c5f811
jxmah <jxmah.23@gmail.com>
2024-12-19 13:13:12 +0100

    class

--- spotifyAPI.py
+++ spotifyAPI.py
@@ -4,37 +4,39 @@ import base64
 from requests import post
 import json
 
-load_dotenv()
+class SpotifyAPI:
+    def __init__(self) -> None:
+        load_dotenv()
+        self.client_id = os.getenv('CLIENT_ID')
+        self.client_secret = os.getenv('CLIENT_SECRET')
+        self.token = None
 
-#Import credentials
-client_id = os.getenv('CLIENT_ID')
-client_secret = os.getenv('CLIENT_SECRET')
+    def get_token(self):
+        '''
+        Get access token from spotify for developers which can be used to
+        access a given resource or user's data. 
 
-def get_token():
-    '''
-    Get access token from spotify for developers which can be used to
-    access a given resource or user's data. 
+        Returns: 
+        token - str
+        '''
+        auth_string = self.client_id + ':' + self.client_secret
+        auth_bytes = auth_string.encode('utf-8')
+        auth_base64 = str(base64.b64encode (auth_bytes), 'utf-8')
 
-    Returns: 
-    token - str
-    '''
-    auth_string = client_id + ':' + client_secret
-    auth_bytes = auth_string.encode('utf-8')
-    auth_base64 = str(base64.b64encode (auth_bytes), 'utf-8')
+        url = 'https://accounts.spotify.com/api/token'
+        headers = {
+            'Authorization': 'Basic ' + auth_base64,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
 
-    url = 'https://accounts.spotify.com/api/token'
-    headers = {
-        'Authorization': 'Basic ' + auth_base64,
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
+        data = {'grant_type': 'client_credentials'}
+        result = post(url, headers=headers, data=data)
+        json_result = json.loads(result.content)
+        token = json_result['access_token']
+        return token
 
-    data = {'grant_type': 'client_credentials'}
-    result = post(url, headers=headers, data=data)
-    json_result = json.loads(result.content)
-    token = json_result['access_token']
-    return token
+    def get_auth_header(token):
+        return {'Authorization': 'Beare ' + token}
 
-def get_auth_header(token):
-    return {'Authorization': 'Beare ' + token}
-
-token = get_token()
+spotifyapi = SpotifyAPI()
+token = spotifyapi.get_token()
