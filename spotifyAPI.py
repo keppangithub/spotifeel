25775a2793efb6645537ae0cb12f2f951357b699
jxmah <jxmah.23@gmail.com>
2024-12-19 14:47:57 +0100

    Update spotifyAPI.py

--- spotifyAPI.py
+++ spotifyAPI.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 import base64
-from requests import post
+from requests import post, get
 import json
 
 class SpotifyAPI:
@@ -32,11 +32,32 @@ class SpotifyAPI:
         data = {'grant_type': 'client_credentials'}
         result = post(url, headers=headers, data=data)
         json_result = json.loads(result.content)
-        token = json_result['access_token']
-        return token
+        self.token = json_result['access_token']
+        return self.token
 
-    def get_auth_header(token):
-        return {'Authorization': 'Beare ' + token}
+    def get_auth_header(self):
+        return {'Authorization': 'Bearer ' + self.token}
+    
+    def search_track(self, track, artist):
+        url = 'https://api.spotify.com/v1/search'
+        headers = self.get_auth_header()
+        query = f'?q=remaster%2520{track}%3ADoxy%2520{artist}%3AMiles%2520Davis&type=track%2Cartist&market=US&limit=1&offset=0'
+
+        query_url = url + query
+        result = get(query_url, headers=headers)
+        json_result = json.loads(result.content)
+
+        if 'tracks' in json_result and json_result['tracks']['items']:
+            track_info = json_result['tracks']['items'][0]
+            track_name = track_info['name']
+            artist_name = track_info['artists'][0]['name']
+
+        if len(json_result) == 0:
+            print('No artist with this name exists.')
+            return None
+        return {'track_name': track_name, 'artist_name': artist_name}
 
 spotifyapi = SpotifyAPI()
 token = spotifyapi.get_token()
+song = spotifyapi.search_track('Baby', 'Justin Bieber')
+print(song)
