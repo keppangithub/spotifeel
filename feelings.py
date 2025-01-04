0836f98fce671c49925b660f052cc5787821c0f4
Munther23 <jonathan.munther@gmail.com>
2025-01-04 12:17:30 +0100

    Confirm feeling html, feeling py

---
+++ feelings.py
@@ -0,0 +1,36 @@
+def get_feelings(feeling):  
+    feelings = {
+        "happy": ["euphoric", "loving", "happy", "Euphoric", "Loving", "Happy"],
+        "angry": ["urious", "frustrated", "Furious", "Frustrated"],
+        "sad": ["horrified", "disappointed", "useless", "regretful", "dejected", "unhappy", "scared", "anxious", "Horrified", "Disappointed", "Useless", "Regretful", "Dejected", "Unhappy", "Dcared", "Anxious"]
+    } 
+    for category, feelings in feelings.items():
+        if feeling in feelings:
+            if category == "happy":
+                return ["Det verkar som du haft en bra dag idag!", "Fortsätt i samma vibe.", "Lugna ner"]
+            if category == "angry":
+                return ["Det verkar som idag har varit en frustrerande dag.", "Muntra upp", "Få ut ilskan (rock/snabb musik)"]
+            if category == "sad":
+                return ["Det verkar som om du haft en jobbig dag.", "Muntra upp", "Grotta i känslan ett tag"]
+
+    return "Feeling not found"
+
+
+def negated_feeling(feeling):
+    feelings = {
+        "happy": ["euphoric", "loving", "happy", "Euphoric", "Loving", "Happy"],
+        "angry": ["urious", "frustrated", "Furious", "Frustrated"],
+        "sad": ["horrified", "disappointed", "useless", "regretful", "dejected", "unhappy", "scared", "anxious", "Horrified", "Disappointed", "Useless", "Regretful", "Dejected", "Unhappy", "Dcared", "Anxious"]
+    } 
+    for category, feelings in feelings.items():
+        if feeling in feelings:
+            if category == "happy":
+                print("Negated category = sad")
+                return "sad"
+            if category == "angry":
+                print("Negated category = chill")
+                return "chill"
+            if category == "sad":
+                print("Negated category = happy")
+                return "happy"
+            
