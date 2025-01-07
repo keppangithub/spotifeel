535d943e4bc45d018b9fa06f8e5f69673363057f
Geemmeet <ellen.gemback@gmail.com>
2025-01-07 12:06:39 +0100

    upd CSS login

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def playlist():
         today = date.today()
         user.get_user_information()
         songs_for_playlist = promptGPT.create_playlist(feeling)
-        new_playlist_id = user.create_new_playlist(user.user_id, f'{feeling.capitalize()} - [{today}]')
+        new_playlist_id = user.create_new_playlist(user.user_id, f'{feeling.capitalize()} - {today}')
         user.add_to_playlist(new_playlist_id, songs_for_playlist)
 
         return render_template('playlist.html')
