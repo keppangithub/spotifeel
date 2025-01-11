a2f6489aedd4290c32fa84025b7f5737d093b257
jxmah <jxmah.23@gmail.com>
2025-01-11 16:05:28 +0100

    completed css

--- main.py
+++ main.py
@@ -114,9 +114,10 @@ def playlist():
         user.get_user_information()
         songs_for_playlist = promptGPT.create_playlist(feeling)
         new_playlist_id = user.create_new_playlist(user.user_id, f'{feeling.capitalize()} - {today}')
-        user.add_to_playlist(new_playlist_id, songs_for_playlist)    
+        user.add_to_playlist(new_playlist_id, songs_for_playlist)
+        display_feeling = feeling.capitalize()    
             
-        return render_template('playlist.html', songs_for_playlist=songs_for_playlist, feeling=feeling, today=today)
+        return render_template('playlist.html', songs_for_playlist=songs_for_playlist, display_feeling=display_feeling, today=today)
 
 
 
